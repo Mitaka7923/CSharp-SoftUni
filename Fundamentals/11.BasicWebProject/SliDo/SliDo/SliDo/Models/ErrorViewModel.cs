4439942788651f9f687ac295cf8f236e25cfabe3
Mitaka7923 <dimitar.sideris@gmail.com>
2024-04-01 00:05:11 +0300

    updateStatus

--- Fundamentals/11.BasicWebProject/SliDo/SliDo/SliDo/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SliDo.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
